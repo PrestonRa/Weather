e1ac37ec5b3cbed5fc5366e548d0261129d0ef34
Preston <preston.ramrattan@gmail.com>
2023-12-04 12:58:48 +0900

    added stations and url works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = df.station(date)
+    filename = "weather_txt/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
@@ -19,4 +22,4 @@ def contact_us():
     return render_template("contact_us.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
